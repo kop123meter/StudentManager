caf3b5837524d61000a99274bf67a30a1fe9635d
kop123meter <83102141+kop123meter@users.noreply.github.com>
2024-04-22 12:59:35 -0400

    first commit

---
+++ src/FileInfoReader.java
@@ -0,0 +1,51 @@
+/**
+ * FileInfoReader
+ * @author Ze Li and Chenwei Tang
+ */
+
+import java.io.File;
+public class FileInfoReader {
+    private String fileName;
+    private String fileType;
+    private String[] fileContent;
+    private int fileLength;
+    
+    public FileInfoReader(String filePath){
+        File file = new File(filePath);
+        fileName = file.getName();
+        fileType = fileName.substring(fileName.lastIndexOf(".")+1);
+        fileLength = (int)file.length();
+        fileContent = new String[fileLength];
+        try {
+            java.util.Scanner input = new java.util.Scanner(file);
+            int i = 0;
+            while (input.hasNext()) {
+                fileContent[i] = input.nextLine();
+                i++;
+            }
+            input.close();
+            fileLength = i;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    
+    }
+
+    public String getFileName() {
+        return fileName;
+    }
+
+    public String getFileType() {
+        return fileType;
+    }
+
+    public String[] getFileContent() {
+        return fileContent;
+    }
+
+    public int getFileLength() {
+        return fileLength;
+    }
+   
+    
+}
---
+++ src/role/AbstractUser.java
@@ -0,0 +1,12 @@
+package role;
+
+/**
+ * Abstract class for User
+ * @author Ze Li and Chenwei Tang
+ */
+abstract class AbstractUser {
+    private String username;
+    private String password;
+    private String role;
+    
+}
